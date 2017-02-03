f1b0cf93ab7aa1d333808b7ae5ad970fbfa14e95
Roman Schmerold (BNoiZe) <bnoize@froxlor.org>
2017-02-03 18:05:37 +0100

    fix some codeclimate

--- index.js
+++ index.js
@@ -9,393 +9,393 @@ const https = require('https');
 const querystring = require('querystring');
 
 const GET = 'GET',
-    POST = 'POST',
-    DELETE = 'DELETE';
+	POST = 'POST',
+	DELETE = 'DELETE';
 
 
 // Public
 module.exports = EasynameApi;
 
 function EasynameApi(id, mail, apiKey, apiAuthSalt, apiSignSalt) {
-    this.id = id;
-    this.mail = mail;
-    this.apiKey = apiKey;
-    this.apiAuthSalt = apiAuthSalt;
-    this.apiSignSalt = apiSignSalt;
+	this.id = id;
+	this.mail = mail;
+	this.apiKey = apiKey;
+	this.apiAuthSalt = apiAuthSalt;
+	this.apiSignSalt = apiSignSalt;
 
-    this.createApiAuthentication = function() {
-        var authentication = vsprintf(this.apiAuthSalt, [this.id, this.mail]);
-        authentication = crypto.createHash('md5').update(authentication).digest("hex");
-        authentication = new Buffer(authentication).toString('base64');
-        return authentication;
-    };
+	this.createApiAuthentication = function() {
+		var authentication = vsprintf(this.apiAuthSalt, [this.id, this.mail]);
+		authentication = crypto.createHash('md5').update(authentication).digest("hex");
+		authentication = new Buffer(authentication).toString('base64');
+		return authentication;
+	};
 
-    this.createBody = function(data = null) {
-        if (!data) {
-            data = [];
-        }
-        var timestamp = +new Date();
+	this.createBody = function(data = null) {
+		if (!data) {
+			data = [];
+		}
+		var timestamp = +new Date();
 
-        var body = {
-            data: data,
-            timestamp: timestamp,
-            signature: this.signRequest(data, timestamp)
-        }
+		var body = {
+			data: data,
+			timestamp: timestamp,
+			signature: this.signRequest(data, timestamp)
+		}
 
-        return JSON.stringify(body);
-    }
+		return JSON.stringify(body);
+	}
 
-    this.signRequest = function(data, timestamp) {
-        var tmpData = JSON.parse(JSON.stringify(data));
-        tmpData['timestamp'] = timestamp;
+	this.signRequest = function(data, timestamp) {
+		var tmpData = JSON.parse(JSON.stringify(data));
+		tmpData['timestamp'] = timestamp;
 
-        var dataKeys = Object.keys(tmpData);
-        dataKeys.sort();
+		var dataKeys = Object.keys(tmpData);
+		dataKeys.sort();
 
-        signData = '';
-        for (var i = 0; i < dataKeys.length; i++) {
-            signData += tmpData[dataKeys[i]];
-        }
+		signData = '';
+		for (var i = 0; i < dataKeys.length; i++) {
+			signData += tmpData[dataKeys[i]];
+		}
 
-        var slice = (signData.length % 2 == 0 ? signData.length / 2 : (signData.length + 1) / 2);
-        var signature = signData.substring(0, slice) + this.apiSignSalt + signData.substring(slice);
-        signature = crypto.createHash('md5').update(signature).digest("hex");
-        signature = new Buffer(signature).toString('base64');
+		var slice = (signData.length % 2 === 0 ? signData.length / 2 : (signData.length + 1) / 2);
+		var signature = signData.substring(0, slice) + this.apiSignSalt + signData.substring(slice);
+		signature = crypto.createHash('md5').update(signature).digest("hex");
+		signature = new Buffer(signature).toString('base64');
 
-        return signature;
-    }
+		return signature;
+	}
 }
 
 EasynameApi.prototype.doRequest = function(method, resource, id = null, subResource = null, subId = null, data = null, perform = null, limit = null, offset = null, filter = null) {
-    var path = '/' + resource;
-    if (id) {
-        path += '/' + id;
-    }
-    if (subResource) {
-        path += '/' + subResource;
-    }
-    if (subId) {
-        path += '/' + subId;
-    }
-    if (perform) {
-        path += '/' + perform;
-    }
+	var path = '/' + resource;
+	if (id) {
+		path += '/' + id;
+	}
+	if (subResource) {
+		path += '/' + subResource;
+	}
+	if (subId) {
+		path += '/' + subId;
+	}
+	if (perform) {
+		path += '/' + perform;
+	}
 
-    var parameters = [];
-    if (method === GET) {
-        if (limit !== null) {
-            parameters['limit'] = limit;
-        }
+	var parameters = [];
+	if (method === GET) {
+		if (limit !== null) {
+			parameters['limit'] = limit;
+		}
 
-        if (offset !== null) {
-            parameters['offset'] = offset;
-        }
+		if (offset !== null) {
+			parameters['offset'] = offset;
+		}
 
-        if (filter !== null) {
-            if (Array.isArray(filter)) {
-                parameters['filter'] = filter.join();
-            } else {
-                parameters['filter'] = filter;
-            }
-        }
+		if (filter !== null) {
+			if (Array.isArray(filter)) {
+				parameters['filter'] = filter.join();
+			} else {
+				parameters['filter'] = filter;
+			}
+		}
 
-        if (Object.keys(parameters).length > 0) {
-            path = path + "?" + querystring.stringify(parameters);
-        }
-    }
+		if (Object.keys(parameters).length > 0) {
+			path = path + "?" + querystring.stringify(parameters);
+		}
+	}
 
-    var options = {
-        hostname: 'api.easyname.com',
-        port: 443,
-        path: path,
-        method: method,
-        headers: {
-            'X-User-ApiKey': this.apiKey,
-            'X-User-Authentication': this.createApiAuthentication(),
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    };
+	var options = {
+		hostname: 'api.easyname.com',
+		port: 443,
+		path: path,
+		method: method,
+		headers: {
+			'X-User-ApiKey': this.apiKey,
+			'X-User-Authentication': this.createApiAuthentication(),
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		}
+	};
 
 
 
-    var req = https.request(options, (res) => {
+	var req = https.request(options, (res) => {
 
-        var body = '';
-        res.on('data', (d) => {
-            body += d;
-        });
+		var body = '';
+		res.on('data', (d) => {
+			body += d;
+		});
 
-        res.on('end', () => {
-            // data reception done, parse json
-            //console.log(body);
-            //console.log(JSON.parse(body));
-        });
-    });
+		res.on('end', () => {
+			// data reception done, parse json
+			//console.log(body);
+			//console.log(JSON.parse(body));
+		});
+	});
 
-    req.on('error', (e) => {
-        console.error(e);
-    });
+	req.on('error', (e) => {
+		console.error(e);
+	});
 
-    // if POST
-    if (method === POST) {
-        console.log(this.createBody(data));
-        //req.write(this.createBody(data));
-    }
+	// if POST
+	if (method === POST) {
+		console.log(this.createBody(data));
+		//req.write(this.createBody(data));
+	}
 
-    req.end();
+	req.end();
 };
 
 /**
  * DOMAIN
  */
 EasynameApi.prototype.getDomain = function(id) {
-    return this.doRequest(GET, 'domain', id);
+	return this.doRequest(GET, 'domain', id);
 };
 
 EasynameApi.prototype.listDomain = function(limit = null, offset = null, filter = null) {
-    return this.doRequest(GET, 'domain', null, null, null, null, null, limit, offset, filter);
+	return this.doRequest(GET, 'domain', null, null, null, null, null, limit, offset, filter);
 };
 
 EasynameApi.prototype.createDomain = function(domain, registrantContact, adminContact, techContact, zoneContact, nameservers, trustee = false) {
-    var data = {
-        domain: domain,
-        registrantContact: registrantContact,
-        adminContact: techContact,
-        techContact: techContact,
-        zoneContact: zoneContact,
-        trustee: (trustee ? 1 : 0),
-        transferIn: 0
-    };
+	var data = {
+		domain: domain,
+		registrantContact: registrantContact,
+		adminContact: techContact,
+		techContact: techContact,
+		zoneContact: zoneContact,
+		trustee: (trustee ? 1 : 0),
+		transferIn: 0
+	};
 
-    for (var i = 0; i < 6; i++) {
-        if (nameservers[i]) {
-            data['nameserver' + (i + 1)] = nameservers[i];
-        }
-    }
+	for (var i = 0; i < 6; i++) {
+		if (nameservers[i]) {
+			data['nameserver' + (i + 1)] = nameservers[i];
+		}
+	}
 
-    return this.doRequest(POST, 'domain', null, null, null, data);
+	return this.doRequest(POST, 'domain', null, null, null, data);
 };
 
 EasynameApi.prototype.transferDomain = function(domain, registrantContact, adminContact, techContact, zoneContact, nameservers, trustee = false, transferAuthcode = null) {
-    var data = {
-        domain: domain,
-        registrantContact: registrantContact,
-        adminContact: adminContact,
-        techContact: techContact,
-        zoneContact: zoneContact,
-        trustee: (trustee ? 1 : 0)
-    }
+	var data = {
+		domain: domain,
+		registrantContact: registrantContact,
+		adminContact: adminContact,
+		techContact: techContact,
+		zoneContact: zoneContact,
+		trustee: (trustee ? 1 : 0)
+	}
 
-    for (var i = 0; i < 6; i++) {
-        if (nameservers[i]) {
-            data['nameserver' + (i + 1)] = nameservers[i];
-        }
-    }
+	for (var i = 0; i < 6; i++) {
+		if (nameservers[i]) {
+			data['nameserver' + (i + 1)] = nameservers[i];
+		}
+	}
 
-    if (transferAuthcode) {
-        data['transferAuthcode'] = transferAuthcode;
-    }
+	if (transferAuthcode) {
+		data['transferAuthcode'] = transferAuthcode;
+	}
 
-    return this.doRequest(POST, 'domain', null, null, null, data);
+	return this.doRequest(POST, 'domain', null, null, null, data);
 };
 
 EasynameApi.prototype.deleteDomain = function(id) {
-    return this.doRequest(POST, 'domain', id, null, null, null, 'delete');
+	return this.doRequest(POST, 'domain', id, null, null, null, 'delete');
 };
 
 EasynameApi.prototype.restoreDomain = function(id) {
-    return this.doRequest(POST, 'domain', id, null, null, null, 'restore');
+	return this.doRequest(POST, 'domain', id, null, null, null, 'restore');
 };
 
 EasynameApi.prototype.expireDomain = function(id) {
-    return this.doRequest(POST, 'domain', id, null, null, null, 'expire');
+	return this.doRequest(POST, 'domain', id, null, null, null, 'expire');
 };
 
 EasynameApi.prototype.unexpireDomain = function(id) {
-    return this.doRequest(POST, 'domain', id, null, null, null, 'expire');
+	return this.doRequest(POST, 'domain', id, null, null, null, 'expire');
 };
 
 EasynameApi.prototype.changeOwnerOfDomain = function(id, registrantContact) {
-    return this.doRequest(POST, 'domain', id, null, null, {
-        registrantContact: registrantContact
-    }, 'ownerchange');
+	return this.doRequest(POST, 'domain', id, null, null, {
+		registrantContact: registrantContact
+	}, 'ownerchange');
 };
 
 EasynameApi.prototype.changeContactOfDomain = function(id, adminContact, techContact, zoneContact) {
-    return this.doRequest(POST, 'domain', id, null, null, {
-        adminContact: adminContact,
-        techContact: techContact,
-        zoneContact: zoneContact
-    }, 'contactchange');
+	return this.doRequest(POST, 'domain', id, null, null, {
+		adminContact: adminContact,
+		techContact: techContact,
+		zoneContact: zoneContact
+	}, 'contactchange');
 };
 
 EasynameApi.prototype.changeNameserverOfDomain = function(id, nameservers) {
-    var data = {};
-    for (var i = 0; i < 6; i++) {
-        if (nameservers[i]) {
-            data['nameserver' + (i + 1)] = nameservers[i];
-        }
-    }
+	var data = {};
+	for (var i = 0; i < 6; i++) {
+		if (nameservers[i]) {
+			data['nameserver' + (i + 1)] = nameservers[i];
+		}
+	}
 
-    return this.doRequest(POST, 'domain', id, null, null, data, 'nameserverchange');
+	return this.doRequest(POST, 'domain', id, null, null, data, 'nameserverchange');
 };
 
 EasynameApi.prototype.generateAuthcodeOfDomain = function(id) {
-    return this.doRequest(POST, 'domain', id, 'generateauthcode');
+	return this.doRequest(POST, 'domain', id, 'generateauthcode');
 };
 
 EasynameApi.prototype.changeTransfersecurityOfDomain = function(id, transferAllowed = false) {
-    return this.doRequest(POST, 'domain', id, 'transfersecurity', null, {
-        transferAllowed: (transferAllowed ? 1 : 0)
-    });
+	return this.doRequest(POST, 'domain', id, 'transfersecurity', null, {
+		transferAllowed: (transferAllowed ? 1 : 0)
+	});
 };
 
 EasynameApi.prototype.autorenewDomain = function(id) {
-    return;
+	return;
 };
 
 /**
  * CONTACT
  */
 EasynameApi.prototype.getContact = function(id) {
-    return this.doRequest(GET, 'contact', id);
+	return this.doRequest(GET, 'contact', id);
 };
 
 EasynameApi.prototype.listContact = function(limit = null, offset = null, filter = null) {
-    return this.doRequest(GET, 'contact', null, null, null, null, null, limit, offset, filter);
+	return this.doRequest(GET, 'contact', null, null, null, null, null, limit, offset, filter);
 };
 
 EasynameApi.prototype.createContact = function(type, alias, name, address, zip, city, country, phone, email, additionalData) {
-    var data = {
-        type: type,
-        alias: alias,
-        name: name,
-        address: address,
-        zip: zip,
-        city: city,
-        country: country,
-        phone: phone,
-        email: email
-    };
+	var data = {
+		type: type,
+		alias: alias,
+		name: name,
+		address: address,
+		zip: zip,
+		city: city,
+		country: country,
+		phone: phone,
+		email: email
+	};
 
-    for (var p in additionalData) {
-        if (additionalData.hasOwnProperty(p)) {
-            data['p'] = additionalData[p];
-        }
-    }
+	for (var p in additionalData) {
+		if (additionalData.hasOwnProperty(p)) {
+			data['p'] = additionalData[p];
+		}
+	}
 
-    return this.doRequest(POST, 'contact', null, null, null, data);
+	return this.doRequest(POST, 'contact', null, null, null, data);
 };
 
 EasynameApi.prototype.updateContact = function(id, alias, address, zip, city, phone, email, additionalData) {
-    var data = {
-        alias: alias,
-        name: name,
-        address: address,
-        zip: zip,
-        city: city,
-        country: country,
-        phone: phone,
-        email: email
-    };
+	var data = {
+		alias: alias,
+		name: name,
+		address: address,
+		zip: zip,
+		city: city,
+		country: country,
+		phone: phone,
+		email: email
+	};
 
-    for (var p in additionalData) {
-        if (additionalData.hasOwnProperty(p)) {
-            data['p'] = additionalData[p];
-        }
-    };
+	for (var p in additionalData) {
+		if (additionalData.hasOwnProperty(p)) {
+			data['p'] = additionalData[p];
+		}
+	}
 
-    return this.doRequest(POST, 'contact', id, null, null, data);
+	return this.doRequest(POST, 'contact', id, null, null, data);
 };
 
 EasynameApi.prototype.deleteContact = function(id) {
-    return this.doRequest(DELETE, 'contact', id);
+	return this.doRequest(DELETE, 'contact', id);
 };
 
 /**
  * DNS
  */
 EasynameApi.prototype.getDns = function(domainId, id) {
-    return this.doRequest(GET, 'domain', domainId, 'dns', id);
+	return this.doRequest(GET, 'domain', domainId, 'dns', id);
 };
 
 EasynameApi.prototype.listDns = function(domainId, limit, offset) {
-    return this.doRequest(GET, 'domain', domainId, 'dns', null, null, null, limit, offset);
+	return this.doRequest(GET, 'domain', domainId, 'dns', null, null, null, limit, offset);
 };
 
 EasynameApi.prototype.createDns = function(domainId, id, name = '', type, content, priority = null, ttl = null) {
-    return;
+	return;
 };
 
 EasynameApi.prototype.updateDns = function(domainId, id, name = '', type, content, priority = null, ttl = null) {
-    return;
+	return;
 };
 
 EasynameApi.prototype.deleteDns = function(domainId, id) {
-    return;
+	return;
 };
 
 /**
  * DATABASE
  */
 EasynameApi.prototype.getDatabase = function(id) {
-    return this.doRequest(GET, 'database', id);
+	return this.doRequest(GET, 'database', id);
 };
 
 EasynameApi.prototype.listDatabase = function(limit, offset) {
-    return this.doRequest(GET, 'database', null, null, null, null, null, limit, offset);
+	return this.doRequest(GET, 'database', null, null, null, null, null, limit, offset);
 };
 
 EasynameApi.prototype.createDatabase = function(password, type = null, notice = null) {
-    return;
+	return;
 };
 
 EasynameApi.prototype.updateDatabase = function(password, type = null, notice = null) {
-    return;
+	return;
 };
 
 /**
  * FTP
  */
 EasynameApi.prototype.getFtpAccount = function(id) {
-    return this.doRequest(GET, 'ftp-account', id);
+	return this.doRequest(GET, 'ftp-account', id);
 };
 
 EasynameApi.prototype.listFtpAccount = function(limit, offset) {
-    return this.doRequest(GET, 'ftp-account', null, null, null, null, null, limit, offset);
+	return this.doRequest(GET, 'ftp-account', null, null, null, null, null, limit, offset);
 };
 
 EasynameApi.prototype.createFtpAccount = function(password, path, notice = null) {
-    return;
+	return;
 };
 
 EasynameApi.prototype.updateFtpAccount = function(password, path, notice = null) {
-    return;
+	return;
 };
 
 /**
  * MAIL
  */
 EasynameApi.prototype.getMailBox = function(id) {
-    return this.doRequest(GET, 'email-box', id);
+	return this.doRequest(GET, 'email-box', id);
 };
 
 EasynameApi.prototype.listMailBox = function(limit, offset) {
-    return this.doRequest(GET, 'email-box', null, null, null, null, null, limit, offset);
+	return this.doRequest(GET, 'email-box', null, null, null, null, null, limit, offset);
 };
 
 EasynameApi.prototype.createMailBox = function(size, password, note = '', antispam = null, antivirus = null, deleteSpam = null) {
-    return;
+	return;
 };
 
 EasynameApi.prototype.updateMailBox = function(size, password, note = null, antispam = null, antivirus = null, deleteSpam = null) {
-    return;
+	return;
 };
 
 /**
  * USER
  */
 EasynameApi.prototype.getUserBalance = function(id) {
-    return this.doRequest(GET, 'user', id, 'balance');
+	return this.doRequest(GET, 'user', id, 'balance');
 }
